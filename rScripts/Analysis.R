c4f01b19abbc8073644e683002cc16b24f6c4438
Henrique Gomide <henriquepgomide@gmail.com>
2015-10-13 17:48:53 -0500

    Fixed problems with bad recode and improved light smoker categories

--- rScripts/Analysis.R
+++ rScripts/Analysis.R
@@ -3,34 +3,25 @@
 # =============================
 # Some notes - Weight - V00291
 
-# Tip - The data is stored as data.table, which is way faster than data.frame. If you do not know how to use data.table, check this out: https://s3.amazonaws.com/assets.datacamp.com/img/blog/data+table+cheat+sheet.pdf
+# Tip 1 - The data is stored as data.table, which is way faster than data.frame. If you do not know how to use data.table, check this out: https://s3.amazonaws.com/assets.datacamp.com/img/blog/data+table+cheat+sheet.pdf
 
-setwd("C:/Users/guilh/pns2013-lightsmokers")
+setwd("C:/Users/guilh/pns2013-lightsmokers") # This works only on Windows. Please comment this line if you're using a real OS.
 
 # Load survey and data.table packages
 library(survey)
 library(data.table)
 
+# set R to produce conservative standard errors instead of crashing
+options(survey.lonely.psu = "adjust")
+
+# Open data set - BE AWARE - data.table format!!
 pnsDT <- readRDS("data/pns.rds")
 
 # Create data.table tabaco without missing data
 #tabaco <- pnsDT[ which(pnsDT$P050 != NA)
     
+# Remove participants who did not filled tobacco survey
 tabaco <- subset(pnsDT, pnsDT$P050 != " ")
-
-tabaco$idade[18 <= tabaco$C008 & tabaco$C008 < 29]<-0
-tabaco$idade[29 <= tabaco$C008 & tabaco$C008 < 59]<-1
-tabaco$idade[59 <= tabaco$C008 & tabaco$C008 < 64]<-2
-tabaco$idade[64 <= tabaco$C008 & tabaco$C008 < 74]<-3
-tabaco$idade[tabaco$C008 >= 74]<-4
-
-
-
-
-tabaco$status[tabaco$P052 == c("3")] <- 0
-tabaco$status[tabaco$P05401 == c("1") & tabaco$P05402 <= 9] <- 1
-tabaco$status[tabaco$P05401 == c("2", "3")] <- 1
-tabaco$status[tabaco$P05401 == c("1") & tabaco$P05402 > 9] <- 2
 
 # Survey format
 fumo <- svydesign(
@@ -39,11 +30,46 @@ fumo <- svydesign(
   weights = ~V00291
 )
 
-# prevalencia de usuario de algum produto de tabaco
+
+#######################################################
+# TEST 1 - PASSED!!!
+# Reproduce the original estimates from IBGE.
+######################################################
+
+# P050 - Tabaco fumado
 prop.table(svytable(formula = ~tabaco$P050, fumo))
+# P067 - Outros produtos que nÃ£o sejam fumados
+prop.table(svytable(formula = ~tabaco$P067, fumo))
 
 
-# Reproduce the original estimate from IBGE.
+######################################################
+# RECODE VARS
+######################################################
+
+## - LIGHT SMOKERS ----#
+
+# Please, think of this categories more carefully. We have to be 100% sure.
+
+tabaco$status[tabaco$P052 == "3"]                           <- "Nunca fumante"
+tabaco$status[tabaco$P05401 == "1" & tabaco$P05402 <= 10]   <- "Fumante leve diario - cig. ind."
+tabaco$status[tabaco$P05401 == "2" | tabaco$P05401 == "3"]  <- "Fumante nao diario - cig. ind." 
+tabaco$status[tabaco$P05401 == "1" & tabaco$P05402 > 10]    <- "Fumante pesado - cig. ind."
+tabaco$status[tabaco$P050 == 3 & 
+                (tabaco$P052 == 1 | tabaco$P052 == 2) ]     <- "Ex-fumante"
+tabaco$status[tabaco$P05401 == 4]                           <- "Fumante esporadico < 1 vez por mes"
+tabaco$status[tabaco$P05401 == 5]                           <- "Nao fumante de cigarro industrializado"
+
+## - AGE ----#
+tabaco$idade[18 <= tabaco$C008 & tabaco$C008 < 29]<- 0
+tabaco$idade[29 <= tabaco$C008 & tabaco$C008 < 59]<- 1
+tabaco$idade[59 <= tabaco$C008 & tabaco$C008 < 64]<- 2
+tabaco$idade[64 <= tabaco$C008 & tabaco$C008 < 74]<- 3
+tabaco$idade[tabaco$C008 >= 74]                   <- 4
+
+######################################################
+# The code below does not work out of the box. Needs
+# to be validated first.
+######################################################
 
 #C006 - Sexo
 prop.table(svytable(formula = ~tabaco$C006+tabaco$status+tabaco$C012, fumo))
@@ -59,42 +85,42 @@ prop.table(svytable(formula = ~tabaco$P050+tabaco$status, fumo ))
 #C012 - O informante desta parte foi:
 prop.table(svytable(~tabaco$C012+tabaco$status, fumo))
 
-#VDD004-N?vel de instrução mais elevado alcançado (pessoas de 5 anos ou mais de idade)
+#VDD004-N?vel de instru??o mais elevado alcan?ado (pessoas de 5 anos ou mais de idade)
 prop.table(svytable(~tabaco$VDD004+tabaco$status, fumo))
 
-#N001-Em geral, como o(a) Sr(a) avalia a sua saúde?
+#N001-Em geral, como o(a) Sr(a) avalia a sua sa?de?
 prop.table(svytable( ~tabaco$N001+tabaco$status, fumo))
 
 
-#P005-A Sra est? grávida no momento?
+#P005-A Sra est? gr?vida no momento?
 prop.table(svytable(~tabaco$P005+tabaco$status, fumo))
 
-#P028-Quantos dias por semana o(a) Sr(a) costuma tomar alguma bebida alcoólica? 
+#P028-Quantos dias por semana o(a) Sr(a) costuma tomar alguma bebida alco?lica? 
 prop.table(svytable(~tabaco$P028+tabaco$status, fumo))
 
-#P029-Em geral, no dia que o(a) Sr(a) bebe, quantas doses de bebida alcoólica o(a) Sr(a) consome?
+#P029-Em geral, no dia que o(a) Sr(a) bebe, quantas doses de bebida alco?lica o(a) Sr(a) consome?
 prop.table(svytable(~tabaco$P029+tabaco$status, fumo))
 
 #P055-Quanto tempo depois de acordar o(a) Sr(a) normalmente fuma pela primeira vez?
 prop.table(svytable(~tabaco$P055+tabaco$status, fumo))
 
 
-#P060-Durante os últimos 12 meses, o(a) Sr(a) tentou parar de fumar?
+#P060-Durante os ?ltimos 12 meses, o(a) Sr(a) tentou parar de fumar?
 prop.table(svytable(~tabaco$P060+tabaco$status, fumo))
 
 
-#P061-Quando o(a) Sr(a) tentou parar de fumar, procurou tratamento com profissional de saúde?
+#P061-Quando o(a) Sr(a) tentou parar de fumar, procurou tratamento com profissional de sa?de?
 prop.table(svytable(~tabaco$P061+tabaco$status, fumo))
 
 
-#P072-Nos últimos 30 dias, as adverténcias nos ma?os de cigarro levaram o(a) Sr(a) a pensar em parar de fumar?
+#P072-Nos ?ltimos 30 dias, as advert?ncias nos ma?os de cigarro levaram o(a) Sr(a) a pensar em parar de fumar?
 prop.table(svytable(~tabaco$P072+tabaco$status, fumo))
 
 
-#Q002-Algum médico já lhe deu o diagnóstico de hipertensão arterial (press?o alta)?
+#Q002-Algum m?dico j? lhe deu o diagn?stico de hipertens?o arterial (press?o alta)?
 prop.table(svytable(~tabaco$Q002+tabaco$status, fumo))
 
-#Q01805-Em algum dos atendimentos para hipertensão, algum médico ou outro profissional de saúde lhe recomendou n?o fumar?
+#Q01805-Em algum dos atendimentos para hipertens?o, algum m?dico ou outro profissional de sa?de lhe recomendou n?o fumar?
 prop.table(svytable(~tabaco$Q01805+tabaco$status, fumo))
 
 #Q030-Algum m?dico j? lhe deu o diagn?stico de diabetes?
@@ -105,57 +131,57 @@ prop.table(svytable(~tabaco$Q030+tabaco$status, fumo))
 prop.table(svytable(~tabaco$Q04604+tabaco$status, fumo))
 
 
-#Q060-Algum médico já lhe deu o diagn?stico de colesterol alto?
+#Q060-Algum m?dico j? lhe deu o diagn?stico de colesterol alto?
 prop.table(svytable(~tabaco$Q060+tabaco$status, fumo))
 
 
-#Q06205-Algum médico ou outro profissional de saúde lhe deu alguma recomendação para não fumar por causa do colesterol alto?
+#Q06205-Algum m?dico ou outro profissional de sa?de lhe deu alguma recomenda??o para n?o fumar por causa do colesterol alto?
 prop.table(svytable(~tabaco$Q06205+tabaco$status, fumo))
 
 
-#Q063-Algum médico já lhe deu o diagnóstico de uma doença do coração tais como infarto, angina, insuficiência cardiaca ou outra?
+#Q063-Algum m?dico j? lhe deu o diagn?stico de uma doen?a do cora??o tais como infarto, angina, insufici?ncia cardiaca ou outra?
 prop.table(svytable(~tabaco$Q063+tabaco$status, fumo))
 
-#Q068-Algum médico já lhe deu o diagnóstico de AVC (Acidente Vascular cerebral) ou derrame?
+#Q068-Algum m?dico j? lhe deu o diagn?stico de AVC (Acidente Vascular cerebral) ou derrame?
 prop.table(svytable(~tabaco$Q068+tabaco$status, fumo))
 
 
-#Q074-Algum médico já lhe deu o diagnóstico de asma (ou bronquite asm?tica)?
+#Q074-Algum m?dico j? lhe deu o diagn?stico de asma (ou bronquite asm?tica)?
 prop.table(svytable(~tabaco$Q074+tabaco$status, fumo))
 
 
-#Q079-Algum médico já lhe deu o diagnóstico de artrite ou reumatismo?
+#Q079-Algum m?dico j? lhe deu o diagn?stico de artrite ou reumatismo?
 prop.table(svytable(~tabaco$Q079+tabaco$status, fumo))
 
 
-#Q084-O(a) Sr(a) tem algum problema crônico de coluna, como dor cr?nica nas costas ou no pescoço, lombalgia, dor ciática, problemas nas v?rtebras ou disco?
+#Q084-O(a) Sr(a) tem algum problema cr?nico de coluna, como dor cr?nica nas costas ou no pesco?o, lombalgia, dor ci?tica, problemas nas v?rtebras ou disco?
 prop.table(svytable(~tabaco$Q084+tabaco$status, fumo))
 
-#Q088-Algum médico já lhe deu o diagnóstico de DORT (distúrbio osteomuscular relacionado ao trabalho)?
+#Q088-Algum m?dico j? lhe deu o diagn?stico de DORT (dist?rbio osteomuscular relacionado ao trabalho)?
 prop.table(svytable(~tabaco$Q088+tabaco$status, fumo))
 
-#Q092-Algum médico ou profissional de saúde mental (como psiquiatra ou psicólogo) já lhe deu o diagnóstico de depressão?
+#Q092-Algum m?dico ou profissional de sa?de mental (como psiquiatra ou psic?logo) j? lhe deu o diagn?stico de depress?o?
 prop.table(svytable(~tabaco$Q092+tabaco$status, fumo))
 
 
-#Q110-Algum m?dico ou profissional de sa?de mental (como psiquiatra ou psic?logo) já lhe deu o diagn?stico de outra doen?a mental, como esquizofrenia, transtorno bipolar, psicose ou TOC (Transtorno obsessivo compulsivo)? PS: O question?rio possui uest?es específicas para cada uma das doen?as.
+#Q110-Algum m?dico ou profissional de sa?de mental (como psiquiatra ou psic?logo) j? lhe deu o diagn?stico de outra doen?a mental, como esquizofrenia, transtorno bipolar, psicose ou TOC (Transtorno obsessivo compulsivo)? PS: O question?rio possui uest?es espec?ficas para cada uma das doen?as.
 prop.table(svytable(~tabaco$Q110+tabaco$status, fumo))
 
-#Q116-Algum médico já lhe deu o diagnóstico de alguma doença no pulm?ão ou DPOC (Doen?a Pulmonar Obstrutiva Cr?nica), tais como enfisema pulmonar, bronquite crônica ou outro?
+#Q116-Algum m?dico j? lhe deu o diagn?stico de alguma doen?a no pulm??o ou DPOC (Doen?a Pulmonar Obstrutiva Cr?nica), tais como enfisema pulmonar, bronquite cr?nica ou outro?
 prop.table(svytable(~tabaco$Q116+tabaco$status, fumo))
 
-#Q120-Algum médico já lhe deu algum diagnóstico de c?ncer?
+#Q120-Algum m?dico j? lhe deu algum diagn?stico de c?ncer?
 prop.table(svytable(~tabaco$Q120+tabaco$status, fumo))
 
-#Q121-No primeiro diagn?stico de câncer, que tipo de c?ncer o(a) sr(a) tem ou teve?
+#Q121-No primeiro diagn?stico de c?ncer, que tipo de c?ncer o(a) sr(a) tem ou teve?
 prop.table(svytable(~tabaco$Q121+tabaco$status, fumo))
 
 
-#Q124-Algum m?dico j? lhe deu o diagn?stico de insuficiência renal crônica?
+#Q124-Algum m?dico j? lhe deu o diagn?stico de insufici?ncia renal cr?nica?
 prop.table(svytable(~tabaco$Q124+tabaco$status, fumo))
 
 
-#Q128-Algum médico já lhe deu o diagnóstico de outra doen?a cr?nica, f?sica ou mental, ou doen?a de longa dura??o (de mais de 6 meses de dura??o)?
+#Q128-Algum m?dico j? lhe deu o diagn?stico de outra doen?a cr?nica, f?sica ou mental, ou doen?a de longa dura??o (de mais de 6 meses de dura??o)?
 prop.table(svytable(~tabaco$Q128+tabaco$status, fumo))
 
 
---
+++ rScripts/irt.R
@@ -0,0 +1,59 @@
+# =============================
+# ANALYSIS
+# =============================
+
+library(data.table)
+library(mokken)
+library(KernSmoothIRT)
+
+pnsDT <- readRDS("data/pns.rds")
+
+
+# =============================
+# PHQ
+# =============================
+
+phq <- pnsDT[, .(N010, N011, N012, N013, N014, N015, N016, N017, N018)]
+
+items <- c("N010", "N011", "N012", "N013", "N014", "N015", "N016", "N017", "N018")
+
+phq[, N010 := ifelse(N010 == " ", NA, N010)]
+phq[, N011 := ifelse(N011 == " ", NA, N011)]
+phq[, N012 := ifelse(N012 == " ", NA, N012)]
+phq[, N013 := ifelse(N013 == " ", NA, N013)]
+phq[, N014 := ifelse(N014 == " ", NA, N014)]
+phq[, N015 := ifelse(N015 == " ", NA, N015)]
+phq[, N016 := ifelse(N016 == " ", NA, N016)]
+phq[, N017 := ifelse(N017 == " ", NA, N017)]
+phq[, N018 := ifelse(N018 == " ", NA, N018)]
+
+phq9 <- as.data.frame(na.omit(phq))
+phq9 <- sapply(phq9, as.numeric)
+
+rm(phq, pnsDT)
+
+# Reliability
+check.reliability(phq9)
+
+# Monotonocity
+monotonicity.results <- check.monotonicity(phq9)
+summary(monotonicity.results)
+plot(monotonicity.results)
+
+# IRT
+phqIRT <- ksIRT(responses =  phq9, key = 4, format = 2)
+phqIRT
+
+
+plot(phqIRT, plottype = "OCC", items = "all")
+plot(phqIRT, plottype = "EIS", items = "all")
+
+plot(phqIRT, plottype = "triangle", items = "all")
+
+plot(phqIRT, plottype = "PCA")
+
+plot(phqIRT, plottype = "RCC", subjects = c(160, 5000, 10000, 15000, 50000))
+
+plot(phqIRT, plottype="expected")
+plot(phqIRT, plottype="sd")
+plot(phqIRT, plottype="density")
