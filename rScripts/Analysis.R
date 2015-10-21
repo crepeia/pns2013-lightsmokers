811a7435e8981bd7f0b38d1d544e2a43f168ac48
Guilherme Marcelino <guilherme1marcelino@gmail.com>
2015-10-21 10:17:28 -0200

    crosstabs and graphics

--- rScripts/Analysis.R
+++ rScripts/Analysis.R
@@ -71,117 +71,81 @@ tabaco$idade[tabaco$C008 >= 74]                   <- 4
 # to be validated first.
 ######################################################
 
-#C006 - Sexo
-prop.table(svytable(formula = ~tabaco$C006+tabaco$status+tabaco$C012, fumo))
-prop.table(svytable(formula = ~tabaco$C006+tabaco$idade, fumo))
+#status x Sexo
+prop.table(svytable(formula = ~tabaco$status+tabaco$C006, fumo))
 
-# P050 - Atualmente, o(a) Sr(a) fuma algum produto do tabaco?
-prop.table(svytable(formula = ~tabaco$P050+tabaco$status, fumo ))
+#status x Região do brasil
+prop.table(svytable(formula = ~tabaco$status+tabaco$V0001,fumo))
 
+#status x faixa etaria 
+prop.table(svytable(formula = ~tabaco$status+tabaco$idade,fumo))
 
-#svychisq(~tabaco$C006+tabaco$status, fumo)
-#svychisq(~tabaco$C006+tabaco$status, fumo,statistic="adjWald")
+#status x escolaridade
+prop.table(svytable(formula = ~tabaco$status+tabaco$VDD004,fumo))
 
-#C012 - O informante desta parte foi:
-prop.table(svytable(~tabaco$C012+tabaco$status, fumo))
-
-#VDD004-N?vel de instru??o mais elevado alcan?ado (pessoas de 5 anos ou mais de idade)
-prop.table(svytable(~tabaco$VDD004+tabaco$status, fumo))
-
-#N001-Em geral, como o(a) Sr(a) avalia a sua sa?de?
-prop.table(svytable( ~tabaco$N001+tabaco$status, fumo))
+#status x hipertensão
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q002,fumo))
 
 
-#P005-A Sra est? gr?vida no momento?
-prop.table(svytable(~tabaco$P005+tabaco$status, fumo))
+#status x diabetes 
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q030,fumo))
 
-#P028-Quantos dias por semana o(a) Sr(a) costuma tomar alguma bebida alco?lica? 
-prop.table(svytable(~tabaco$P028+tabaco$status, fumo))
+#status x doença ranal cronica 
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q124,fumo))
 
-#P029-Em geral, no dia que o(a) Sr(a) bebe, quantas doses de bebida alco?lica o(a) Sr(a) consome?
-prop.table(svytable(~tabaco$P029+tabaco$status, fumo))
+#status x asma 
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q074,fumo))
 
-#P055-Quanto tempo depois de acordar o(a) Sr(a) normalmente fuma pela primeira vez?
-prop.table(svytable(~tabaco$P055+tabaco$status, fumo))
+#status x DPOC
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q116,fumo))
 
+#status x câncer
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q120,fumo))
 
-#P060-Durante os ?ltimos 12 meses, o(a) Sr(a) tentou parar de fumar?
-prop.table(svytable(~tabaco$P060+tabaco$status, fumo))
-
-
-#P061-Quando o(a) Sr(a) tentou parar de fumar, procurou tratamento com profissional de sa?de?
-prop.table(svytable(~tabaco$P061+tabaco$status, fumo))
+#status x câncer de pulmão
+prop.table(svytable(formula = ~tabaco$status+tabaco$Q121,fumo))
 
 
-#P072-Nos ?ltimos 30 dias, as advert?ncias nos ma?os de cigarro levaram o(a) Sr(a) a pensar em parar de fumar?
-prop.table(svytable(~tabaco$P072+tabaco$status, fumo))
+######################################################
+#### GRAPHICS ??
+######################################################
+
+#Status x sexo
+
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$C006, fumo)),beside = TRUE,names.arg = c("Masculino","Feminino"),main = "Status x Sexo")
 
 
-#Q002-Algum m?dico j? lhe deu o diagn?stico de hipertens?o arterial (press?o alta)?
-prop.table(svytable(~tabaco$Q002+tabaco$status, fumo))
+#status x Região do brasil
 
-#Q01805-Em algum dos atendimentos para hipertens?o, algum m?dico ou outro profissional de sa?de lhe recomendou n?o fumar?
-prop.table(svytable(~tabaco$Q01805+tabaco$status, fumo))
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$V0001,fumo)),beside = TRUE)
 
-#Q030-Algum m?dico j? lhe deu o diagn?stico de diabetes?
-prop.table(svytable(~tabaco$Q030+tabaco$status, fumo))
+#status x faixa etaria 
 
-
-#Q04604-Em algum dos atendimentos para diabetes, algum m?dico ou outro profissional de sa?de lhe recomendou n?o fumar?
-prop.table(svytable(~tabaco$Q04604+tabaco$status, fumo))
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$idade,fumo)),beside = TRUE,main = "Status x Faixa etária")
 
 
-#Q060-Algum m?dico j? lhe deu o diagn?stico de colesterol alto?
-prop.table(svytable(~tabaco$Q060+tabaco$status, fumo))
+#Status x Escolaridade
 
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$VDD004,fumo)),beside = TRUE,main = "Status x Escolaridade ")
 
-#Q06205-Algum m?dico ou outro profissional de sa?de lhe deu alguma recomenda??o para n?o fumar por causa do colesterol alto?
-prop.table(svytable(~tabaco$Q06205+tabaco$status, fumo))
+#Status x hipertensão
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q002,fumo)),beside = TRUE, main = "Status x Hipertensão",names.arg = c("não aplicavel","sim","apenas durante a gravidez","não "))
 
+#status x diabetes 
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q030,fumo)),beside = TRUE,main = "Status x Diabetes",names.arg = c("não aplicavel","sim","apenas durante a gravidez","não "))
 
-#Q063-Algum m?dico j? lhe deu o diagn?stico de uma doen?a do cora??o tais como infarto, angina, insufici?ncia cardiaca ou outra?
-prop.table(svytable(~tabaco$Q063+tabaco$status, fumo))
+#status x doença ranal cronica 
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q124,fumo)),beside = TRUE,main = "Status x doença renal Crônica",names.arg = c("sim","não"))
 
-#Q068-Algum m?dico j? lhe deu o diagn?stico de AVC (Acidente Vascular cerebral) ou derrame?
-prop.table(svytable(~tabaco$Q068+tabaco$status, fumo))
+#status x asma 
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q074,fumo)),beside = TRUE,main = "Status x Asma",names.arg = c("sim","não"))
 
+#status x DPOC 
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q116,fumo)),beside = TRUE,main = "Status x DPOC",names.arg = c("sim","não"))
 
-#Q074-Algum m?dico j? lhe deu o diagn?stico de asma (ou bronquite asm?tica)?
-prop.table(svytable(~tabaco$Q074+tabaco$status, fumo))
+#status x câncer
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q120,fumo)),beside = TRUE,main = "Status x Câncer",names.arg = c("sim","não"))
 
-
-#Q079-Algum m?dico j? lhe deu o diagn?stico de artrite ou reumatismo?
-prop.table(svytable(~tabaco$Q079+tabaco$status, fumo))
-
-
-#Q084-O(a) Sr(a) tem algum problema cr?nico de coluna, como dor cr?nica nas costas ou no pesco?o, lombalgia, dor ci?tica, problemas nas v?rtebras ou disco?
-prop.table(svytable(~tabaco$Q084+tabaco$status, fumo))
-
-#Q088-Algum m?dico j? lhe deu o diagn?stico de DORT (dist?rbio osteomuscular relacionado ao trabalho)?
-prop.table(svytable(~tabaco$Q088+tabaco$status, fumo))
-
-#Q092-Algum m?dico ou profissional de sa?de mental (como psiquiatra ou psic?logo) j? lhe deu o diagn?stico de depress?o?
-prop.table(svytable(~tabaco$Q092+tabaco$status, fumo))
-
-
-#Q110-Algum m?dico ou profissional de sa?de mental (como psiquiatra ou psic?logo) j? lhe deu o diagn?stico de outra doen?a mental, como esquizofrenia, transtorno bipolar, psicose ou TOC (Transtorno obsessivo compulsivo)? PS: O question?rio possui uest?es espec?ficas para cada uma das doen?as.
-prop.table(svytable(~tabaco$Q110+tabaco$status, fumo))
-
-#Q116-Algum m?dico j? lhe deu o diagn?stico de alguma doen?a no pulm??o ou DPOC (Doen?a Pulmonar Obstrutiva Cr?nica), tais como enfisema pulmonar, bronquite cr?nica ou outro?
-prop.table(svytable(~tabaco$Q116+tabaco$status, fumo))
-
-#Q120-Algum m?dico j? lhe deu algum diagn?stico de c?ncer?
-prop.table(svytable(~tabaco$Q120+tabaco$status, fumo))
-
-#Q121-No primeiro diagn?stico de c?ncer, que tipo de c?ncer o(a) sr(a) tem ou teve?
-prop.table(svytable(~tabaco$Q121+tabaco$status, fumo))
-
-
-#Q124-Algum m?dico j? lhe deu o diagn?stico de insufici?ncia renal cr?nica?
-prop.table(svytable(~tabaco$Q124+tabaco$status, fumo))
-
-
-#Q128-Algum m?dico j? lhe deu o diagn?stico de outra doen?a cr?nica, f?sica ou mental, ou doen?a de longa dura??o (de mais de 6 meses de dura??o)?
-prop.table(svytable(~tabaco$Q128+tabaco$status, fumo))
-
+#status x tipo de câncer
+barplot(prop.table(svytable(formula = ~tabaco$status+tabaco$Q121,fumo)),beside = TRUE,main = "Status x tipo de Câncer ")
 
