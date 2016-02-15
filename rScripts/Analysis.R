53314c554aee659af0778b86168353138c626420
taynaraformagini <taynaraformagini@gmail.com>
2016-02-15 17:41:18 -0200

    Added all the other tables.

--- rScripts/Analysis.R
+++ rScripts/Analysis.R
@@ -25,19 +25,20 @@ options(survey.lonely.psu = "adjust")
 # Open data set - BE AWARE - data.table format!!
 pnsDT <- readRDS("data/pns.rds")
 
-    
+
 # Remove participants who did not filled tobacco survey
 tabaco <- subset(pnsDT, pnsDT$P050 != " ")
 
 # Survey format
 fumo <- svydesign(
-  id = ~1,
+  id = tabaco$UPA,
+  strata = tabaco$V0024,
   data = tabaco,
   weights = ~V00291
 )
 
 #######################################################
-# TEST 1 - 
+# TEST 1 -
 # Reproduce the original estimates from IBGE.
 ######################################################
 
@@ -54,20 +55,20 @@ prop.table(svytable(formula = ~tabaco$P067, fumo))
 ## - LIGHT SMOKERS ----#
 
 ##Recoding all selected participants that answere the individual questionnaire.
-tabaco$status[tabaco$P052 == "3"]                           <- 0  #"Nunca fumante"
+tabaco$status[tabaco$P052 == "3"]                         <- 0  #"Nunca fumante"
 tabaco$status[tabaco$P05401 == "2" | tabaco$P05401 == "3"| tabaco$P05401 == "4" ]  <- 1 #"Fumante nao diario - cig. ind."
 tabaco$status[tabaco$P05401 == "1" & tabaco$P05402 <= 10]   <- 2  #"Fumante leve diario - cig. ind."
 
-tabaco$status[tabaco$P05401 == "1" & tabaco$P05402 > 10]    <- 3   # "Fumante pesado - cig. ind."
-tabaco$status[tabaco$P050 == 3 & (tabaco$P052 == 1 | tabaco$P052 == 2) ]     <- 4 #"Ex-fumante"
-tabaco$status[tabaco$P05401 == 5]                           <- 5 #"Nao fumante de cigarro industrializado"
+tabaco$status[tabaco$P05401 == "1" & tabaco$P05402 > 10]	<- 3   # "Fumante pesado - cig. ind."
+tabaco$status[tabaco$P050 == 3 & (tabaco$P052 == 1 | tabaco$P052 == 2) ] 	<- 4 #"Ex-fumante"
+tabaco$status[tabaco$P05401 == 5]                       	<- 5 #"Nao fumante de cigarro industrializado"
 
 
 ##FERNANDO - to be done
 # status.names <- c("Nunca Fumante", "Fum. não diário", ...)
 
 ##PREVALENCE OF THE 5 GROUPS
-svymean(~tabaco$status, fumo)*100 
+svymean(~tabaco$status, fumo)*100
 
 
 ## - AGE ----#
@@ -75,7 +76,7 @@ tabaco$idade[tabaco$C008>=18 & tabaco$C008 < 29]<- 0
 tabaco$idade[tabaco$C008>=29 & tabaco$C008 < 59]<- 1
 tabaco$idade[tabaco$C008>=59 & tabaco$C008 < 64]<- 2
 tabaco$idade[tabaco$C008>=64 & tabaco$C008 < 74]<- 3
-tabaco$idade[tabaco$C008 >= 74]                 <- 4
+tabaco$idade[tabaco$C008 >= 74]             	<- 4
 
 
 # Recoding the states into 5 Regions (North, Northest, Central West, Southest and South).
@@ -89,6 +90,8 @@ tabaco$regiao[tabaco$V0001 == "50"  | tabaco$V0001 == "51"  | tabaco$V0001 == "5
 # to be validated first.
 ######################################################
 
+
+####### SOCIODEMOGRAPHIC DATA - tables ##########
 # Status x gender
 round(prop.table(svytable(formula = ~tabaco$status+tabaco$C006,fumo), margin = 2),3)*100
 round(prop.table(svytable(formula = ~tabaco$C006+tabaco$status,fumo), margin = 2),3)*100
@@ -100,7 +103,7 @@ round(prop.table(svytable(formula = ~tabaco$status+tabaco$V0001,fumo), margin = 
 round(prop.table(svytable(formula = ~tabaco$status+tabaco$regiao,fumo), margin = 2), 3)*100
 round(prop.table(svytable(formula = ~tabaco$regiao + tabaco$status,fumo), margin = 2), 3)*100
 
-#status x age 
+#status x age
 round(prop.table(svytable(formula = ~tabaco$status+tabaco$idade,fumo), margin = 2), 3)*100
 round(prop.table(svytable(formula = ~tabaco$idade+tabaco$status, fumo), margin = 2),3)*100
 
@@ -110,34 +113,143 @@ round(prop.table(svytable(formula = ~tabaco$status+tabaco$VDD004,fumo), margin =
 round(prop.table(svytable(formula = ~tabaco$VDD004+tabaco$status,fumo), margin = 2),3)*100
 
 
-#TABLES - ILLNESS
+####### ILLNESS - tables ########
 
 #status x hipertensao
+
+round(prop.table(svytable(formula = ~tabaco$Q002+tabaco$status,fumo), margin=2), 3)*100
 has <- round(prop.table(svytable(formula = ~tabaco$Q002+tabaco$status,fumo), margin=2), 3)*100
 has_t <- round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q002,fumo), margin=2), 3)*100
 
-#status x diabetes 
+#status x diabetes
+round(prop.table(svytable(formula = ~tabaco$Q030+tabaco$status,fumo), margin=2), 3)*100
 dm <- round(prop.table(svytable(formula = ~tabaco$Q030+tabaco$status,fumo), margin=2), 3)*100
 dm_t <- round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q030,fumo), margin=2), 3)*100
 
-#status x doenca renal cronica 
+#status x doenca renal cronica
+round(prop.table(svytable(formula = ~tabaco$Q124+tabaco$status,fumo), margin=2),3)*100
 drc <- round(prop.table(svytable(formula = ~tabaco$Q124+tabaco$status,fumo), margin=2),3)*100
 drc_t <- round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q124,fumo), margin=2), 3)*100
 
-#status x asma 
+#status x asma
+round(prop.table(svytable(formula = ~tabaco$Q074+tabaco$status,fumo), margin=2),3)*100
 asma <- round(prop.table(svytable(formula = ~tabaco$Q074+tabaco$status,fumo), margin=2),3)*100
 
-#status x DPOC
+#status x DPOC (or emphysema, chronic bronchitis or other)
+round(prop.table(svytable(formula = ~tabaco$Q116+tabaco$status,fumo), margin=2),3)*100
 dpoc <- round(prop.table(svytable(formula = ~tabaco$Q116+tabaco$status,fumo), margin=2),3)*100
 
-#status x cancer # Do not use for now.
+
+#status x cancer
 round(prop.table(svytable(formula = ~tabaco$Q120+tabaco$status,fumo), margin = 2),3)*100
 cancer <- round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q120,fumo), margin = 2),3)*100
-                
+
 #status x cancer de pulmao
-# OBS: CANCER DE PULMAO - RESPOSTA 1 
+# OBS: CANCER DE PULMAO - RESPOSTA 1
 round(prop.table(svytable(formula = ~tabaco$Q121+tabaco$status,fumo), margin = 2),5)*100
 lung <- round(prop.table(svytable(formula = ~tabaco$Q121+tabaco$status,fumo), margin = 2),5)*100
+
+#status x depression
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q092,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$Q092+tabaco$status,fumo), margin = 2),3)*100
+
+#status x sleeping pills
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q132,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$Q132+tabaco$status,fumo), margin = 2),3)*100
+
+#status x heart illness
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q063,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$Q063+tabaco$status,fumo), margin = 2),3)*100
+
+#status x AVC
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$Q068,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$Q068+tabaco$status,fumo), margin = 2),3)*100
+
+###### health status - table #####
+#status x heath status
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$N001,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$N001+tabaco$status,fumo), margin = 2),3)*100
+
+
+##### life style - alcohol + physical activity) ########
+#status x alcohol use - 1
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P027,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P027+tabaco$status,fumo), margin = 2),3)*100
+
+#status x alcohol use  - 2  #THINK ABOUT THIS ONE
+
+
+
+##recoding variable P028 according to AUDIT 
+
+tabaco$diasemana[tabaco$P028==0  | tabaco$P028==1] <- 0   #0 ou 1 vez/semana
+tabaco$diasemana[tabaco$P028==2  | tabaco$P028==3] <- 1 #2 ou 3 /semana
+tabaco$diasemana[tabaco$P028==4  | tabaco$P028==5 | tabaco$P028==6 | tabaco$P028==7] <- 4 ou mais/semana
+
+
+round(prop.table(svytable(formula = ~tabaco$diasemana+tabaco$status,fumo), margin = 2),3)*100
+
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P028,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P028+tabaco$status,fumo), margin = 2),3)*100
+
+#status x alcohol use - 3 # USED QUESTION 2 OF AUDIT TO COMBINE VARIABLES
+
+#recoding variable P029 according to AUDIT 
+tabaco$dose[tabaco$P029==1 | tabaco$P029 ==2]<- 0
+tabaco$dose[tabaco$P029==3 | tabaco$P029 ==4]<- 1
+tabaco$dose[tabaco$P029==5 | tabaco$P029 ==6]<- 2
+tabaco$dose[tabaco$P029==7 | tabaco$P029 ==9]<- 3
+tabaco$dose[tabaco$P029>= 10] <-4
+
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P029,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$dose+tabaco$status,fumo), margin = 2),3)*100
+
+#status x physical activity
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P034,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P034+tabaco$status,fumo), margin = 2),3)*100
+
+
+
+##### tobacco use - tables ########
+#status x tobacco use: history of smoking
+
+#recoding variable P053 according to IBGE graphic
+
+tabaco$idadeinicio[tabaco$P053<=14] <- 0 #menor ou igual a 14
+tabaco$idadeinicio[tabaco$P053>=14 & tabaco$P053<=19] <- 1 #15-19 anos
+tabaco$idadeinicio[tabaco$P053>=20 & tabaco$P053<=24] <-  2 #20-24 anos
+tabaco$idadeinicio[tabaco$P053>=25 & tabaco$P053<=29] <- 3 #25-29 anos
+tabaco$idadeinicio[tabaco$P053>=30 & tabaco$P053<=34] <- 4 #30 a 34 anos
+tabaco$idadeinicio[tabaco$P053>=35 & tabaco$P053<=39] <- 5 #35 a 39 anos
+tabaco$idadeinicio[tabaco$P053>=40] <- 6 #40 ou mais.
+
+
+round(prop.table(svytable(formula = ~tabaco$idadeinicio+tabacostatus,fumo), margin = 2),3)*100
+
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P053,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P053+tabaco$status,fumo), margin = 2),3)*100
+
+#status x tobacco use: how long after wake up, first cigarette
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P055,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P055+tabaco$status,fumo), margin = 2),3)*100
+
+#status x tobacco use: cessation
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P060,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P060+tabaco$status,fumo), margin = 2),3)*100
+
+#status x tobacco use: treatment
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P061,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P061+tabaco$status,fumo), margin = 2),3)*100
+
+#status x tobacco use: secondhand smoking
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P068,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P068+tabaco$status,fumo), margin = 2),3)*100
+
+#status x tobacco use: antitobacco marketing
+round(prop.table(svytable(formula = ~tabaco$status+tabaco$P072,fumo), margin = 2),3)*100
+round(prop.table(svytable(formula = ~tabaco$P072+tabaco$status,fumo), margin = 2),3)*100
+
+
 
 ######################################################
 #### GRAPHICS
@@ -153,12 +265,12 @@ fig3 <- melt(fig3, id.vars="doencas") # Melt dataFrame to plot on Ggplot, requir
 fig3$variable <- revalue(fig3$variable, c("X0"="Nunca Fumante", "X1"="Fumante não diário", "X2"="Fumante Leve", "X3"="Fumante Pesado", "X4"="Ex-fumante")) # Insert names
 
 # Plot graph
-ggplot(fig3, aes(x = doencas, y = value, fill=variable)) + # Insert plot basic parameters 
+ggplot(fig3, aes(x = doencas, y = value, fill=variable)) + # Insert plot basic parameters
   geom_bar(stat="identity", position="dodge") +  # Barplot
   theme_minimal(base_size = 14, base_family = "Arial") + #Font size and Font Family
   xlab("") + ylab("%") + #xlabel and ylabel
   theme(legend.position = "bottom", legend.direction="horizontal",    
-          legend.background = element_rect(colour = NA, fill = "white")) + # Postion legend and fill its background with white.
+        legend.background = element_rect(colour = NA, fill = "white")) + # Postion legend and fill its background with white.
   scale_fill_manual(name="", values = brewer.pal(5, "OrRd")) # Fix legend name and add a better colour pallette
 
 
@@ -177,11 +289,11 @@ fig4 <- melt(fig4, id.vars="doencas") # Melt dataFrame to plot on Ggplot, requir
 fig4$variable <- revalue(fig4$variable, c("X0"="Nunca Fumante", "X1"="Fumante não diário", "X2"="Fumante Leve", "X3"="Fumante Pesado", "X4"="Ex-fumante")) # Insert names
 
 # Plot graph
-ggplot(fig4, aes(x = doencas, y = value, fill=variable)) + # Insert plot basic parameters 
-     geom_bar(stat="identity", position="dodge") +  # Barplot
-     theme_minimal(base_size = 14, base_family = "Arial") + #Font size and Font Family
-     xlab("") + ylab("%") + #xlabel and ylabel
-     theme(legend.position = "bottom", legend.direction="horizontal", 
-           legend.background = element_rect(colour = NA, fill = "white")) + # Postion legend and fill its background with white.
-     scale_fill_manual(name="", values = brewer.pal(5, "OrRd")) # Fix legend name and add a better colour pallette
+ggplot(fig4, aes(x = doencas, y = value, fill=variable)) + # Insert plot basic parameters
+  geom_bar(stat="identity", position="dodge") +  # Barplot
+  theme_minimal(base_size = 14, base_family = "Arial") + #Font size and Font Family
+  xlab("") + ylab("%") + #xlabel and ylabel
+  theme(legend.position = "bottom", legend.direction="horizontal",
+        legend.background = element_rect(colour = NA, fill = "white")) + # Postion legend and fill its background with white.
+  scale_fill_manual(name="", values = brewer.pal(5, "OrRd")) # Fix legend name and add a better colour pallette
 
